aa1f2bbeac01bb39c810e7d593df2516643794a7
avneeetk <avneet15khanna@gmail.com>
2024-10-12 00:05:25 +0530

    5

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,6 +1,5 @@
 import express from "express";
 import {
-  getUserDetails,
   login,
   logoutAdmin,
   logoutPatient,
@@ -15,7 +14,6 @@ const router = express.Router();
 
 router.post("/patient/register", patientRegister);
 router.post("/login", login);
-router.get("/patient/me", isPatientAuthenticated, getUserDetails);
 router.get("/admin/me", isAdminAuthenticated, getUserDetails);
 router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
 router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
