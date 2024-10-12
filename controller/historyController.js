3c2dd73cd1f7ed523907e858e4fc4af641fda1e9
avneeetk <161209821+avneeetk@users.noreply.github.com>
2024-10-12 22:24:50 +0530

    Update historyController.js

--- controller/historyController.js
+++ controller/historyController.js
@@ -1,6 +1,7 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import { PatientHistory } from "../models/historySchema.js";
 import ErrorHandler from "../middlewares/error.js";
+import XLSX from 'xlsx';
 
 // Function to post patient history
 export const postPatientHistory = catchAsyncErrors(async (req, res, next) => {
@@ -46,7 +47,7 @@ export const postPatientHistory = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-// Function to get all patient histories
+// Function to get all patient histories (accessible to all authenticated users)
 export const getAllPatientHistories = catchAsyncErrors(async (req, res, next) => {
   const histories = await PatientHistory.find().populate('patientId', 'firstName lastName email phone dob');
 
@@ -56,12 +57,38 @@ export const getAllPatientHistories = catchAsyncErrors(async (req, res, next) =>
   });
 });
 
-// Function to export patient histories
-export const exportPatientHistories = catchAsyncErrors(async (req, res, next) => {
-  const histories = await PatientHistory.find(); // Fetch all histories
+// Function to export patient histories as Excel
+export const exportHistories = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const histories = await PatientHistory.find();
 
-  res.status(200).json({
-    success: true,
-    histories, // Send histories for export
-  });
+    // Transform the data as needed
+    const data = histories.map(history => ({
+      firstName: history.firstName,
+      lastName: history.lastName,
+      dob: history.dob,
+      gender: history.gender,
+      amyloidosisTypes: history.amyloidosisTypes.join(", "),
+      symptoms: history.symptoms.join(", "),
+      treatmentHistory: history.treatmentHistory,
+      currentMedications: history.currentMedications.join(", "),
+      familyHistory: history.familyHistory,
+      lifestyleFactors: history.lifestyleFactors.join(", "),
+    }));
+
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Histories");
+    const buffer = XLSX.write(workbook, { bookType: "xlsx", type: "buffer" });
+
+    // Set response headers
+    res.setHeader("Content-Disposition", "attachment; filename=patient_histories.xlsx");
+    res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    
+    // Send the Excel file
+    res.send(buffer);
+  } catch (error) {
+    return next(new ErrorHandler("Failed to export histories", 400));
+  }
 });
+
