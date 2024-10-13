882bc9a61e076e5a720910a7aeded1fbd67bc845
avneeetk <161209821+avneeetk@users.noreply.github.com>
2024-10-14 01:01:32 +0530

    Update historyRouter.js

--- router/historyRouter.js
+++ router/historyRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { postPatientHistory, getAllPatientHistories, exportHistories } from "../controller/historyController.js";
-// import { isPatientAuthenticated } from "../middlewares/auth.js"; // Import the authentication middleware
+import { isPatientAuthenticated } from "../middlewares/auth.js"; // Import the authentication middleware
 
 const router = express.Router();
 
 // POST request for adding patient history
-// router.post("/add", isPatientAuthenticated, postPatientHistory); // Add the authentication middleware here
-router.post("/add",  postPatientHistory); // Add the authentication middleware here
+router.post("/add", isPatientAuthenticated, postPatientHistory); // Add the authentication middleware here
+ // Add the authentication middleware here
 
 // GET request for fetching all histories
 router.get("/", getAllPatientHistories);
