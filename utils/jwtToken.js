a35b5ba6c958ac36dbce31da44b98165bff4c35f
avneeetk <161209821+avneeetk@users.noreply.github.com>
2024-10-19 12:54:32 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,17 +1,14 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
-  // Determine the cookie name based on the user's role
   const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
 
   res
     .status(statusCode)
     .cookie(cookieName, token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
+      expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      secure: true,
-      sameSite:"None"
+      secure: process.env.NODE_ENV === 'production',  // Secure only in production
+      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',  // Set sameSite depending on environment
     })
     .json({
       success: true,
