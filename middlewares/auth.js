56d3068df368a455826c0d28560e83c24a8ced2c
avneeetk <161209821+avneeetk@users.noreply.github.com>
2024-10-19 12:40:03 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ import { catchAsyncErrors } from "./catchAsyncErrors.js";
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
-// Admin Authentication
+// Middleware to authenticate the admin based on static credentials
 export const isAdminAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const token = req.cookies.adminToken;
   if (!token) {
@@ -13,18 +13,19 @@ export const isAdminAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   req.user = await User.findById(decoded.id);
 
+  // Since the admin is hardcoded, you don't need a role check, just ensure the token is valid
   if (!req.user) {
     return next(new ErrorHandler("Invalid token or user not found", 403));
   }
-
-  next();
+  
+  next(); // Continue to the next middleware
 });
 
-// Patient Authentication
+// Middleware to authenticate all users (who are patients)
 export const isPatientAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const token = req.cookies.patientToken;
   if (!token) {
-    return next(new ErrorHandler("User is not authenticated!", 401));
+    return next(new ErrorHandler("User is not authenticated!", 400));
   }
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
@@ -34,28 +35,6 @@ export const isPatientAuthenticated = catchAsyncErrors(async (req, res, next) =>
     return next(new ErrorHandler("Invalid token or user not found", 403));
   }
 
+  // Proceed as all users are treated as patients
   next();
 });
-jwtToken.js
-export const generateToken = (user, message, statusCode, res) => {
-  const token = user.generateJsonWebToken();
-  // Determine the cookie name based on the user's role
-  const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
-
-  res
-    .status(statusCode)
-    .cookie(cookieName, token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-      secure: true,
-      sameSite:"None"
-    })
-    .json({
-      success: true,
-      message,
-      user,
-      token,
-    });
-};
