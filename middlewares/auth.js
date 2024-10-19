60ef3d26adcf3e83bc0c6a5eb851f4266167d519
avneeetk <161209821+avneeetk@users.noreply.github.com>
2024-10-19 12:28:41 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,45 @@
+import { User } from "../models/userSchema.js";
+import { catchAsyncErrors } from "./catchAsyncErrors.js";
+import ErrorHandler from "./error.js";
+import jwt from "jsonwebtoken";
+
+// Admin Authentication
+export const isAdminAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const token = req.cookies.adminToken;
+  if (!token) {
+    return next(new ErrorHandler("Admin is not authenticated!", 400));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  req.user = await User.findById(decoded.id);
+
+  if (!req.user) {
+    return next(new ErrorHandler("Invalid token or user not found", 403));
+  }
+
+  next();
+});
+
+// Patient Authentication
+export const isPatientAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const token = req.cookies.patientToken;
+  if (!token) {
+    return next(new ErrorHandler("User is not authenticated!", 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  req.user = await User.findById(decoded.id);
+
+  if (!req.user) {
+    return next(new ErrorHandler("Invalid token or user not found", 403));
+  }
+
+  next();
+});
+jwtToken.js
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
+  // Determine the cookie name based on the user's role
   const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
 
   res
@@ -9,8 +49,8 @@ export const generateToken = (user, message, statusCode, res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',  // Secure cookie only in production
-      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',  // Adjust sameSite based on environment
+      secure: true,
+      sameSite:"None"
     })
     .json({
       success: true,
